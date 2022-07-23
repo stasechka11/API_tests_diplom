22a16fdcc497872bb65147ff889888f54b99c860
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-23 14:33:56 +0300

    order api client

---
+++ src/main/java/ru/yandex/practicum/stellarburger/api/OrderClient.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.stellarburger.api;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import ru.yandex.practicum.stellarburger.api.model.order.AvailableIngredients;
+import ru.yandex.practicum.stellarburger.api.model.order.Order;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderClient extends BaseApiClient {
+    public static final String BASE_PATH_ORDER = "/api/orders";
+
+    @Step("Get available ingredients")
+    public AvailableIngredients getAvailableIngredients() {
+        return
+                given()
+                        .spec(getReqSpec())
+                        .when()
+                        .log().all()
+                        .get(BASE_URL + "/api/ingredients")
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .and()
+                        .extract().as(AvailableIngredients.class);
+    }
+
+    @Step
+    public Response createOrder(String accessToken, Order order) {
+        return
+                given()
+                        .spec(getReqSpec())
+                        .header("Authorization", accessToken)
+                        .body(order)
+                        .when()
+                        .log().all()
+                        .post(BASE_URL + BASE_PATH_ORDER);
+    }
+}
