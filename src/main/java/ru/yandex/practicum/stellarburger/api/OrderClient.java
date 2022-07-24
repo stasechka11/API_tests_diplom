c56e87a7c749b4ab1862456013f41cf4711a0121
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-24 12:41:18 +0300

    Added method for taking random N ingredients

--- src/main/java/ru/yandex/practicum/stellarburger/api/OrderClient.java
+++ src/main/java/ru/yandex/practicum/stellarburger/api/OrderClient.java
@@ -5,10 +5,14 @@ import io.restassured.response.Response;
 import ru.yandex.practicum.stellarburger.api.model.order.AvailableIngredients;
 import ru.yandex.practicum.stellarburger.api.model.order.Order;
 
+import java.util.Collections;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 public class OrderClient extends BaseApiClient {
     public static final String BASE_PATH_ORDER = "/api/orders";
+    public static final String NO_INGREDIENTS_MESSAGE = "Ingredient ids must be provided";
 
     @Step("Get available ingredients")
     public AvailableIngredients getAvailableIngredients() {
@@ -25,7 +29,7 @@ public class OrderClient extends BaseApiClient {
                         .extract().as(AvailableIngredients.class);
     }
 
-    @Step
+    @Step("Create order {order}")
     public Response createOrder(String accessToken, Order order) {
         return
                 given()
@@ -35,5 +39,11 @@ public class OrderClient extends BaseApiClient {
                         .when()
                         .log().all()
                         .post(BASE_URL + BASE_PATH_ORDER);
+    }
+
+    @Step("Get list with random {idCount} ingredients")
+    public List<String> getRandomNIngredientIds(int idCount, List<String> ingredientsIdsList) {
+        Collections.shuffle(ingredientsIdsList);
+        return ingredientsIdsList.subList(0, idCount);
     }
 }
